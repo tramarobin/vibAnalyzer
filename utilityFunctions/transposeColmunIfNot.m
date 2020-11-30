91dd72215f55e8ce79ef8d9b33b560c20ac694ce
tramarobin <56997117+tramarobin@users.noreply.github.com>
2020-11-30 12:09:06 +0100

    Update transposeColmunIfNot.m

--- utilityFunctions/transposeColmunIfNot.m
+++ utilityFunctions/transposeColmunIfNot.m
@@ -1,6 +1,6 @@
 function y=transposeColmunIfNot(x)
 
-if min(size(x)==1) % 1 vector
+if min(size(x))==1 % 1 vector
     if size(x,2)>1
         y=transpose(x);
     else
@@ -9,11 +9,11 @@ if min(size(x)==1) % 1 vector
     
 else % several vectors
     
-    if size(x,2)> size(x,1)
-        y=transpose(x);
-    else
-        y=x;
-    end
+    %     if size(x,2)> size(x,1)
+    %         y=transpose(x);
+    %     else
+    y=x;
+    %     end
 end
 
 end
